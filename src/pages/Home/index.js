ec2f90174e66897859e368ca9b77b1736b795498
Trung <ldtrung@IfiSolution.local>
2020-01-30 17:26:23 +0700

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import RootComponent from '@/components/RootComponent/index';
+
+export class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+
+
+  render() {
+    return (
+      <RootComponent />
+    );
+  }
+}
+
+export default Home;
+
